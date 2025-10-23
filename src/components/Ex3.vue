fbb92ba7610bd1b6b3c38732dbe24bf8694972e6
feliciaor2024-arch <felicia.or.2024@smu.edu.sg>
2025-10-23 14:09:54 +0000

    Ex1-3

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,32 +1,69 @@
 <script>
-    export default { 
+export default {
 
-       // add code here
-
+  // add code here
+  data() {
+    return {
+      moods: ['Happy', 'Sad', 'Angry'],
+      subject: '',
+      entry: '',
+      selMood: '',
+      outputMsg: '',
     }
+  },
+  computed: {
+    baseUrl() {
+      if (window.location.hostname == 'localhost')
+        return 'http://localhost:3000'
+      else {
+        const codespace_host = window.location.hostname.replace('5173', '3000')
+        return `https://${codespace_host}`;
+      }
+    }
+  },
+  methods: {
+    addPost() {
+      axios.get(`${this.baseUrl}/addPost`, { // backtick is used when we have variable + string together
+        params: {
+          'subject': this.subject,
+          'entry': this.entry,
+          'mood': this.selMood
+        }
+      }) .then(response=> {
+        this.outputMsg = response.data.message
+      }) .catch(error=> {
+        console.log("Error!")
+      })
+    }
+  }
+
+}
 </script>
 
 <template>
-    <div class="table m-2">
-        <h3>Add a New Blog Post</h3>
+  <div class="table m-2">
+    <h3>Add a New Blog Post</h3>
 
-        Subject: <input type='text' size='30' v-model='subject' required>
-        <br>
+    Subject: <input type='text' size='30' v-model='subject' required>
+    <br>
 
-        Entry: <br>
-        <textarea name='entry' cols='80' rows='5' v-model='entry' required></textarea>
-        <br>
+    Entry: <br>
+    <textarea name='entry' cols='80' rows='5' v-model='entry' required></textarea>
+    <br>
 
-        Mood:
-        <!-- TODO: Build a dropdown list here for selecting the mood -->
+    Mood:
+    <!-- TODO: Build a dropdown list here for selecting the mood -->
+    <select v-model = "selMood">
+      <option v-for="mood in moods">{{ mood }}</option>
+    </select>
 
-        <br>
+    <br>
 
-        <br>
-        <button>Submit New Post</button>
+    <br>
+    <button @click="addPost">Submit New Post</button>
+    {{outputMsg}}
 
-        <hr> Click  <a><router-link to="/ViewPosts/">here</router-link></a>  to return to Main Page
-       
-    </div>
+    <hr> Click <a><router-link to="/ViewPosts/">here</router-link></a> to return to Main Page
+
+  </div>
 </template>
-
